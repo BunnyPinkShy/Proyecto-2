4faee06a70a41368336400f814e09ab3505dc0d6
bunny <euphiebunny@gmail.com>
2022-08-04 12:18:07 -0400

    proyecto 2 terminado

---
+++ scrip.js
@@ -0,0 +1,10 @@
+document.write(`<h1> ʚïɞ Princess Fluttershy ʚïɞ</h1>
+    <img width="300" src="https://64.media.tumblr.com/e2207940ca19ffb3e5e630a052df14aa/3ffc00c77d9f8f90-4b/s400x600/c59e8a5685f697b734cce21a43efb0d08ec0f504.jpg" alt="">
+    <h2> ꒰ఎ Little ʚïɞ Shy ʚïɞ Cute ໒꒱</h2>
+    <h2> Kindness is contagius ~ ! ໒꒰ྀི˵ˊ ᵕ ˋ˵꒱ྀི১ </h2>
+    <h2> ♡ ୧ ‧₊ Animals and Plants are the best friends</h2>
+
+    <iframe width="300" height="300" src="https://bunnypinkshy.tumblr.com/" frameborder="0"></iframe>
+    <iframe width="300" height="300" src="https://www.youtube.com/embed/dTif1wHTFfs" frameborder="0"></iframe>
+    <iframe widht="300" height="300" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d831438.4633291869!2d139.2093851493426!3d35.509062672317945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x605d1b87f02e57e7%3A0x2e01618b22571b89!2zVG9raW8sIEphcMOzbg!5e0!3m2!1ses!2sve!4v1659133862821!5m2!1ses!2sve" frameborder="0"></iframe>
+    <iframe src="https://cdn.pixabay.com/audio/2022/05/11/audio_7f6adc457c.mp3" frameborder="0"></iframe> ` )
